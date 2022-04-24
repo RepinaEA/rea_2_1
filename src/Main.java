a898fd57f56001f490ea21e7f52b37fd5fbf5e0f
Elena Repina <repina_ea@mail.ru>
2022-04-24 19:55:39 +0300

    задание_2.1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1000;
+        int bonusMile = 20;
+
+        System.out.println("Количество бонусных миль: " + (price / bonusMile));
+    }
+}
